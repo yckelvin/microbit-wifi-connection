e0aae657ba2b8dd2122d1a9ca0e3288a6bdd2085
Kelvin Lee <yckelvin@users.noreply.github.com>
2021-07-18 16:08:08 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+microIoT.microIoT_initDisplay()
+let wifi_name = "DGINCB_WT6F"
+microIoT.microIoT_WIFI(wifi_name, "20210601")
+microIoT.microIoT_MQTT(
+"vkW338gnR",
+"DkZq38gnRz",
+"DanDCZznR",
+microIoT.SERVERS.English
+)
+microIoT.microIoT_showUserText(0, wifi_name)
